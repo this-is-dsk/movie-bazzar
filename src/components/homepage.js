1470208c66c2a704839bc9c76db7ad8f086f139e
this-is-dsk <yadavdilkhush80@gmail.com>
2023-08-24 09:21:11 +0530

    first setup initiated

---
+++ src/components/homepage.js
@@ -0,0 +1,24 @@
+import DrawerAppBar from "./common/header"
+
+function HomePage(){
+
+    return <>
+    <div>
+    <DrawerAppBar/>
+<h1>Featured Movies</h1>
+
+<img src="23458.jpg"></img>
+
+
+
+
+    </div>
+    </>
+}
+
+
+
+export default HomePage
+
+
+
